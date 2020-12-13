f0e4b73a9a8894be5b5d7ad2997f25e404510afd
kalathbi <74098071+kalathbi@users.noreply.github.com>
2020-12-12 21:18:52 -0700

    Compound Exp

---
+++ CompoundBoolean/CompoundBoolean/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace CompoundBoolean
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(77, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "True-False:A Boolean game";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.button1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(112, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.button3.ForeColor = System.Drawing.SystemColors.Info;
+            this.button3.Location = new System.Drawing.Point(61, 296);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(55, 55);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "True";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.button4.ForeColor = System.Drawing.SystemColors.Info;
+            this.button4.Location = new System.Drawing.Point(229, 296);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 55);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "False";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button5.ForeColor = System.Drawing.SystemColors.Control;
+            this.button5.Location = new System.Drawing.Point(122, 368);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 40);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Restart";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.Location = new System.Drawing.Point(134, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(81, 232);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(175, 45);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ CompoundBoolean/CompoundBoolean/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompoundBoolean
+{
+    public partial class Form1 : Form
+    {
+        public int score = 0;
+        
+        public static Color[] colorlist = { Color.Red, Color.Yellow, Color.Gray, Color.Green };
+        public static  String[] colors = { "Red", "Yellow", "Gray", "Green" };
+        public static String[] logic = { " || ", " && "};
+        public static String[] notlogic = { " ! ", " " };
+        private static readonly Random random = new Random();
+        private static readonly Random random2 = new Random();
+        public bool winstatus;
+
+        private static readonly object syncLock = new object();
+        public static int RandomNumber(int min, int max)
+        {
+            lock (syncLock)
+            { // synchronize
+                return random.Next(min, max);
+            }
+        }
+
+        public static int newRandomNumber(int min, int max)
+        {
+            lock (syncLock)
+            { // synchronize
+                return random2.Next(min, max);
+            }
+        }
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            button3.BackColor = Color.DeepSkyBlue;
+            button4.BackColor = Color.DeepSkyBlue;
+            button5.BackColor = Color.Red;
+            button1.Text = "Score = " + score;
+            
+            initialize();
+
+            
+        }
+
+        public void initialize()
+        {
+            button3.Enabled = true;
+            button4.Enabled = true;
+            int number = RandomNumber(1,4);
+            int randomcolor = newRandomNumber(0,4);
+            button2.BackColor = colorlist[randomcolor];
+            button2.Text = number.ToString();
+            int colornum = RandomNumber(0, 4);
+            int randomsymbol = RandomNumber(0, 2);
+            int boolnumber = newRandomNumber(0, 2);
+            int otherbool = RandomNumber(0, 2);
+            int evaluate = RandomNumber(1, 5);
+
+            string frontlogic = notlogic[boolnumber];
+            string colorname = colors[colornum];
+            string symbol = logic[randomsymbol];
+            string numberlogic = notlogic[otherbool];
+            string testnumber = evaluate.ToString();
+            button6.Text = "( " + frontlogic + colorname + symbol + numberlogic + testnumber + " )";
+
+            winstatus = false;
+
+
+            if (frontlogic == " " && numberlogic == " ")
+            {
+                if (symbol == " && " && randomcolor == colornum && number == evaluate)
+                {
+                    winstatus = true;
+                }
+                else if (symbol == " || " && (randomcolor == colornum || number == evaluate))
+                {
+                    winstatus = true;
+                }
+            }
+            else if (frontlogic == " ! " && numberlogic == " ! ")
+            {
+                if (symbol == " && " && randomcolor != colornum && number != evaluate)
+                {
+                    winstatus = true;
+                }
+                else if (symbol == " || " && (randomcolor != colornum || number != evaluate))
+                {
+                    winstatus = true;
+                }
+            }
+            else if (frontlogic == " ! " && numberlogic == " ")
+            {
+                if (symbol == " && " && randomcolor != colornum && number == evaluate)
+                {
+                    winstatus = true;
+                }
+                else if (symbol == " || " && (randomcolor != colornum || number == evaluate))
+                {
+                    winstatus = true;
+                }
+          
+            }
+            else if (frontlogic == " " && numberlogic == " ! ")
+            {
+                if(symbol == " && " && randomcolor == colornum && number != evaluate)
+                {
+                    winstatus = true;
+                }
+                else if(symbol == " || " && (randomcolor == colornum || number != evaluate))
+                {
+                    winstatus = true;
+                }
+            }
+            
+
+            
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e) // Restart Button
+        {
+            score = 0;
+            button1.Text = "Score = " + score;
+            button3.BackColor = Color.DeepSkyBlue;
+            button4.BackColor = Color.DeepSkyBlue;
+            initialize();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(winstatus == true)
+            {
+                score = score + 1;
+                button1.Text = "Score = " + score;
+                initialize();
+            }
+            else if(winstatus == false)
+            {
+                button3.Enabled = false;
+                button4.Enabled = false;
+                
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (winstatus == false)
+            {
+                score = score + 1;
+                button1.Text = "Score = " + score;
+                initialize();
+            }
+            else if (winstatus == true)
+            {
+                button3.Enabled = false;
+                button4.Enabled = false;
+            }
+
+        }
+
+        
+    }
+}
